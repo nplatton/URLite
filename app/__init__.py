1eb5c4cbbeaeb169b8924defa3e8cbab320b6c1f
Noah Platton <noahplatton@gmail.com>
2022-03-30 17:45:51 +0100

    Minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
+# from db_config import Config
+
 app = Flask(__name__)
 CORS(app)
+# app.config.from_object(Config)
 
 from app import routes
 from app import errors
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 import pyshorteners
 
 from app import app
@@ -12,7 +12,7 @@ def home():
   if request.method == "GET":
     return render_template('home.html')
   if request.method == "POST":
-    long_url = request.form['url']
+    long_url = request.form['url-field']
     s = pyshorteners.Shortener()
     tiny_url = s.tinyurl.short(long_url)
     short_url_code = tiny_url.split("/")[-1]
@@ -21,3 +21,11 @@ def home():
     return render_template('result.html', url=short_url)
   
 # To expand to original URL use .tinyurl.expand(short_url)
+
+@app.route("/<string:path>")
+def redirect():
+  # Check if it is in the database
+  # IF in db redirect
+  # else:
+  #   return redirect("/")
+  pass
